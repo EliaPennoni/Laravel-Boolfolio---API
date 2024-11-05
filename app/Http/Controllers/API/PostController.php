28da2d366c10ab450cb3e55a50a133d2e223979f
EliaPennoni <elia.pennoni1991@gmail.com>
2024-11-05 16:28:49 +0100

    crezione controller per chiamata API

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// models
+use App\Models\Project;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::get();
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'ok',
+            'data' => [
+                'projects' => $projects
+            ],
+
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// controllers
+use App\Http\Controllers\API\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::name('api.')->group(function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/post', [PostController::class, 'index'])->name('posts.index');
 });
